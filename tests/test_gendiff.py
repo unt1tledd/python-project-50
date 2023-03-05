37e250848fd528f19867474d5f077c22fd736c09
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-05 15:23:20 +0300

    Update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -13,28 +13,22 @@ yaml3 = 'tests/fixtures/yaml/file3.yaml'
 yaml4 = 'tests/fixtures/yaml/file4.yaml'
 
 
-correct_diff = open('tests/fixtures/correct/correct_diff.txt', 'r')
-correct_diff = correct_diff.read()
-
-correct_stylish = open('tests/fixtures/correct/correct_stylish.txt', 'r')
-correct_stylish = correct_stylish.read()
-
-correct_plain = open('tests/fixtures/correct/correct_plain.txt', 'r')
-correct_plain = correct_plain.read()
-
-correct_json  = json.dumps(json.load(open('tests/fixtures/correct/correct_json.json')), indent=4)
+correct_diff = 'tests/fixtures/correct/correct_diff.txt'
+correct_stylish = 'tests/fixtures/correct/correct_stylish.txt'
+correct_plain = 'tests/fixtures/correct/correct_plain.txt'
+correct_json  = 'tests/fixtures/correct/correct_json.json'
 
 
 @pytest.mark.parametrize("test_input1,test_input2,formatter,expected",
                          [
-                             pytest.param(json1, json2, 'stylish', correct_diff),
-                             pytest.param(yaml1, yaml2, 'stylish', correct_diff),
-                             pytest.param(json3, json4, 'stylish', correct_stylish),
-                             pytest.param(yaml3, yaml4, 'stylish', correct_stylish),
-                             pytest.param(json3, json4, 'plain', correct_plain),
-                             pytest.param(yaml3, yaml4, 'plain', correct_plain),
-                             pytest.param(json3, json4, 'json', correct_json),
-                             pytest.param(yaml3, yaml4, 'json', correct_json)
+                             pytest.param(json1, json2, 'stylish', open(correct_diff, 'r').read()),
+                             pytest.param(yaml1, yaml2, 'stylish', open(correct_diff, 'r').read()),
+                             pytest.param(json3, json4, 'stylish', open(correct_stylish, 'r').read()),
+                             pytest.param(yaml3, yaml4, 'stylish', open(correct_stylish, 'r').read()),
+                             pytest.param(json3, json4, 'plain', open(correct_plain, 'r').read()),
+                             pytest.param(yaml3, yaml4, 'plain', open(correct_plain, 'r').read()),
+                             pytest.param(json3, json4, 'json', json.dumps(json.load(open(correct_json)), indent=4)),
+                             pytest.param(yaml3, yaml4, 'json', json.dumps(json.load(open(correct_json)), indent=4))
                          ]
                          )
 def test_generate_diff(test_input1, test_input2, formatter, expected):
