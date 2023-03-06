2d459179ac4577a851d9e122087237eefc14e557
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-06 15:12:18 +0300

    Update plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -15,19 +15,19 @@ def plain_format(diff, route=''):
         if len(route) > 1:
             k = route + '.' + d['key']
         if status == 'nested':
-            result.extend(plain_format(d['value'], d['key']))
+            result.extend(plain_format(d['value'], k))
         elif status == 'changed':
             old = format_value(d['old'])
             new = format_value(d['new'])
             result.append(
-                f"Property '{d['key']}' was updated. "
+                f"Property '{k}' was updated. "
                 f"From {old} to {new}\n")
         elif status == 'added':
             value = format_value(d['value'])
             result.append(
-                    f"Property '{d['key']}' was added with value: {value}\n")
+                    f"Property '{k}' was added with value: {value}\n")
         elif status == 'deleted':
             result.append(
-                    f"Property '{d['key']}' was removed\n")
+                    f"Property '{k}' was removed\n")
     result = ''.join(result)
     return result
