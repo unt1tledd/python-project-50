f24aefe156412feb8c8136d991b159255b6fda25
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-06 15:00:21 +0300

    Update stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -13,19 +13,19 @@ def format_value(data, depth=1):
 
 def stylish_format(diff, depth=1):
     result = []
-    for k, v in diff.items():
-        status = v['status']
+    for d in diff:
+        status = d['status']
         if status == 'nested':
-            result.append(f"{'   ' * depth}{k}: {{\n"
-                          f"{stylish_format(v['value'], depth + 2)}")
+            result.append(f"{'   ' * depth}{d['key']}: {{\n"
+                          f"{stylish_format(d['value'], depth + 2)}")
             result.append(f"{'  ' * (depth + 1)}}}\n")
         elif status == 'changed':
-            result.append(f"{'  ' * depth}- {k}: "
-                          f"{format_value(v['old'], depth + 2)}\n")
-            result.append(f"{'  ' * depth}+ {k}: "
-                          f"{format_value(v['new'], depth + 2)}\n")
+            result.append(f"{'  ' * depth}- {d['key']}: "
+                          f"{format_value(d['old'], depth + 2)}\n")
+            result.append(f"{'  ' * depth}+ {d['key']}: "
+                          f"{format_value(d['new'], depth + 2)}\n")
         else:
-            result.append(f"{'  ' * depth}{STATUS[status]} {k}: "
-                          f"{format_value(v['value'], depth + 2)}\n")
+            result.append(f"{'  ' * depth}{STATUS[status]} {d['key']}: "
+                          f"{format_value(d['value'], depth + 2)}\n")
     result = ''.join(result)
     return result
