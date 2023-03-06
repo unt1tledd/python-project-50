b754834de9356f24c3cc0218feb062257fb9e3fe
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-06 14:58:19 +0300

    Update maker_diff.py

--- gendiff/maker_diff.py
+++ gendiff/maker_diff.py
@@ -1,23 +1,19 @@
 def make_diff(old_dict, new_dict):
-    result = {}
-    deleted_keys = old_dict.keys() - new_dict.keys()
-    for key in deleted_keys:
-        result[key] = {'status': 'deleted', 'value': old_dict[key]}
-    unchanged_keys = new_dict.keys() & old_dict.keys()
-    added_keys = new_dict.keys() - old_dict.keys()
-    for key in added_keys:
-        result[key] = {'status': 'added', 'value': new_dict[key]}
-    for key in sorted(unchanged_keys):
-        old_value = old_dict[key]
-        new_value = new_dict[key]
-        if isinstance(old_value, dict) and isinstance(new_value, dict):
-            result[key] = {'status': 'nested',
-                           'value': make_diff(old_value, new_value)}
-        elif old_value == new_value:
-            result[key] = {'status': 'unchanged',
-                           'value': new_value}
-        elif old_value != new_value:
-            result[key] = {'status': 'changed',
-                           'old': old_value,
-                           'new': new_value}
-    return dict(sorted(result.items()))
+    result = []
+    all_key = old_dict.keys() | new_dict.keys()
+    for key in sorted(all_key):
+        if key in old_dict and not key in new_dict:
+            result.extend({'status': 'deleted', 'key': key, 'value': old_dict[key]})
+        elif key in new_dict and not key in old_dict:
+            result.append({'status': 'added', 'key': key, 'value': new_dict[key]})
+        elif key in new_dict and key in old_dict:
+            old = old_dict[key]
+            new = new_dict[key]
+            if isinstance(old, dict) and isinstance(new, dict):
+                result.append({'status': 'nested', 'key': key,
+                               'value': make_diff(old, new)})
+            elif old == new:
+                result.append({'status': 'unchanged', 'key': key, 'value': old})
+            else:
+                result.append({'status': 'changed', 'key': key, 'old': old, 'new': new})
+    return result
