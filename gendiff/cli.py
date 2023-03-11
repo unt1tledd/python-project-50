592cec1b657f77894a6b8979cbfb99e40b68f049
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-11 16:34:24 +0300

    Update cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -4,11 +4,8 @@ import yaml
 
 def conversed(data):
     correct_values = {None: 'null', True: 'true', False: 'false'}
-    for k, v in data.items():
-        if isinstance(v, dict):
-            conversed(v)
-        elif isinstance(v, (bool, type(None))):
-            data[k] = correct_values[v]
+    if isinstance(data, (bool, type(None))):
+        data = correct_values[data]
     return data
 
 
