5b0af7d6f950214fbdbebb799041bac8ecc34cc0
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-11 16:54:01 +0300

    Update maker_diff.py

--- gendiff/maker_diff.py
+++ gendiff/maker_diff.py
@@ -3,20 +3,20 @@ def make_diff(old_dict, new_dict):
     all_key = old_dict.keys() | new_dict.keys()
     for key in sorted(all_key):
         if key not in new_dict:
-            result.append({'status': 'deleted',
-                           'key': key, 'value': old_dict[key]})
+            result.append({'key': key, 'status': 'deleted',
+                           'value': old_dict[key]})
         elif key not in old_dict:
-            result.append({'status': 'added',
-                           'key': key, 'value': new_dict[key]})
+            result.append({'key': key, 'status': 'added',
+                           'value': new_dict[key]})
         elif key in new_dict and key in old_dict:
             old = old_dict[key]
             new = new_dict[key]
             if isinstance(old, dict) and isinstance(new, dict):
-                result.append({'status': 'nested', 'key': key,
+                result.append({'key': key, 'status': 'nested'
                                'value': make_diff(old, new)})
             elif old == new:
-                result.append({'status': 'unchanged', 'key': key, 'value': old})
+                result.append({'key': key, 'status': 'unchanged', 'value': old})
             else:
-                result.append({'status': 'changed',
-                               'key': key, 'old': old, 'new': new})
+                result.append({'key': key, 'status': 'changed',
+                               'old': old, 'new': new})
     return result
