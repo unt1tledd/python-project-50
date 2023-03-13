55f2955633524934757470f9c5185b7ec5c33de0
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-13 12:44:49 +0300

    Update generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,4 +1,4 @@
-from gendiff.cli import reading_file
+from gendiff.cli import reading_file, parsed
 from gendiff.maker_diff import make_diff
 from gendiff.formatters.stylish import stylish_format
 from gendiff.formatters.plain import plain_format
@@ -7,7 +7,9 @@ from gendiff.formatters.json import json_format
 
 def generate_diff(filepath1, filepath2, name='stylish'):
     file1 = reading_file(filepath1)
+    file1 = parsed(file1)
     file2 = reading_file(filepath2)
+    file2 = parsed(file2)
     diff = make_diff(file1, file2)
     if name == 'stylish':
         diff = f"{{\n{stylish_format(diff)}}}\n"
