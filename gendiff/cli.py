e8115596c39aa4b80bb5e155171ab1aeadcb507f
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-13 12:43:59 +0300

    Update cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -12,19 +12,19 @@ def conversed(data):
 def reading_file(filepath):
     extension = filepath.split('.')
     if extension[1] == 'yaml' or extension[1] == 'yml':
-        file = reading_yaml(filepath)
+        file = open(filepath)
+        format = 'yaml'
     elif extension[1] == 'json':
-        file = reading_json(filepath)
+        file = open(filepath)
+        format = 'json'
     else:
         raise Exception("Unsupported type of file")
-    return file
+    return file, format
 
 
-def reading_yaml(filepath):
-    file = yaml.safe_load(open(filepath))
-    return file
-
-
-def reading_json(filepath):
-    file = json.load(open(filepath))
+def parsed(file, format):
+    if format == 'json':
+        file = json.load(file)
+    else:
+        file = yaml.safe_load(file)
     return file
