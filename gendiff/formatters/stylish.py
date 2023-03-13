f0b7d47898ff2ac3b32e8c0324eb2ef65762ad9e
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-13 12:07:52 +0300

    Update stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -9,28 +9,28 @@ def format_value(data, depth=1):
     if not isinstance(data, (dict, list)):
         return conversed(data)
     elif isinstance(data, dict):
-        for k, v in data.items():
-            v = conversed(v)
-            result.append(f"\n{'  ' * depth} {k}: {format_value(v, depth + 2)}")
+        for key, value in data.items():
+            value = conversed(value)
+            result.append(f"\n{'  ' * depth} {key}: {format_value(value, depth + 2)}")
             result.append(f"\n{'  ' * (depth - 1)}}}")
     return ''.join(result)
 
 
 def stylish_format(diff, depth=1):
     result = []
-    for d in diff:
-        status = d['status']
+    for dictionary in diff:
+        status = dictionary['status']
         if status == 'nested':
-            result.append(f"{'   ' * depth}{d['key']}: {{\n"
-                          f"{stylish_format(d['value'], depth + 2)}")
+            result.append(f"{'   ' * depth}{dictionary['key']}: {{\n"
+                          f"{stylish_format(dictionary['value'], depth + 2)}")
             result.append(f"{'  ' * (depth + 1)}}}\n")
         elif status == 'changed':
-            result.append(f"{'  ' * depth} - {d['key']}: "
-                          f"{format_value(d['old'], depth + 2)}\n")
-            result.append(f"{'  ' * depth} + {d['key']}: "
-                          f"{format_value(d['new'], depth + 2)}\n")
+            result.append(f"{'  ' * depth} - {dictionary['key']}: "
+                          f"{format_value(dictionary['old'], depth + 2)}\n")
+            result.append(f"{'  ' * depth} + {dictionary['key']}: "
+                          f"{format_value(dictionary['new'], depth + 2)}\n")
         else:
-            result.append(f"{'  ' * depth} {STATUS[status]} {d['key']}: "
-                          f"{format_value(d['value'], depth + 2)}\n")
+            result.append(f"{'  ' * depth} {STATUS[status]} {dictionary['key']}: "
+                          f"{format_value(dictionary['value'], depth + 2)}\n")
     result = ''.join(result)
     return result
