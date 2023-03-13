d9038e73a2272b9f1307814489018312fb7d6e81
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-13 12:20:57 +0300

    Update stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -9,28 +9,29 @@ def format_value(data, depth=1):
     if not isinstance(data, (dict, list)):
         return conversed(data)
     elif isinstance(data, dict):
-        for key, value in data.items():
-            value = conversed(value)
-            result.append(f"\n{'  ' * depth} {key}: {format_value(value, depth + 2)}")
-            result.append(f"\n{'  ' * (depth - 1)}}}")
-    return ''.join(result)
+        for key, val in data.items():
+            val = conversed(val)
+            result.append(f"{'  ' * depth} {key}: {format_value(val, depth + 2)}")
+            result.append(f"{'  ' * (depth - 1)}}}")
+    return '\n'.join(result)
 
 
 def stylish_format(diff, depth=1):
     result = []
     for dictionary in diff:
         status = dictionary['status']
+        key = dictionary['key']
         if status == 'nested':
-            result.append(f"{'   ' * depth}{dictionary['key']}: {{\n"
+            result.append(f"{'   ' * depth}{key}: {{\n"
                           f"{stylish_format(dictionary['value'], depth + 2)}")
             result.append(f"{'  ' * (depth + 1)}}}\n")
         elif status == 'changed':
-            result.append(f"{'  ' * depth} - {dictionary['key']}: "
+            result.append(f"{'  ' * depth} - {key}: "
                           f"{format_value(dictionary['old'], depth + 2)}\n")
-            result.append(f"{'  ' * depth} + {dictionary['key']}: "
+            result.append(f"{'  ' * depth} + {key}: "
                           f"{format_value(dictionary['new'], depth + 2)}\n")
         else:
-            result.append(f"{'  ' * depth} {STATUS[status]} {dictionary['key']}: "
+            result.append(f"{'  ' * depth} {STATUS[status]} {key}: "
                           f"{format_value(dictionary['value'], depth + 2)}\n")
     result = ''.join(result)
     return result
