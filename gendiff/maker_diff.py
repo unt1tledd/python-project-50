c2691bd13464709939aff301dfce38240571467d
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-14 10:42:20 +0300

    Update maker_diff.py

--- gendiff/maker_diff.py
+++ gendiff/maker_diff.py
@@ -8,15 +8,17 @@ def make_diff(old_dict, new_dict):
         elif key not in old_dict:
             result.append({'key': key, 'status': 'added',
                            'value': new_dict[key]})
+        elif isinstance(old, dict) and isinstance(new, dict):
+            old = old_dict[key]
+            new = new_dict[key]
+            result.append({'key': key, 'status': 'nested',
+                           'value': make_diff(old, new)})
+        elif old_dict[key] == new_dict[key]:
+            old = old_dict[key]
+            result.append({'key': key, 'status': 'unchanged', 'value': old})
         else:
             old = old_dict[key]
             new = new_dict[key]
-            if isinstance(old, dict) and isinstance(new, dict):
-                result.append({'key': key, 'status': 'nested',
-                               'value': make_diff(old, new)})
-            elif old == new:
-                result.append({'key': key, 'status': 'unchanged', 'value': old})
-            else:
-                result.append({'key': key, 'status': 'changed',
-                               'old': old, 'new': new})
+            result.append({'key': key, 'status': 'changed',
+                            'old': old, 'new': new})
     return result
