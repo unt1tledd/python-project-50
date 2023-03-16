b89ebbbed3aa8415c0aaf5b9073ba012aed7f6ff
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-16 10:47:44 +0300

    Update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -25,15 +25,17 @@ def read(filepath):
 
 @pytest.mark.parametrize("test_input1,test_input2,formatter,expected",
                          [
-                             pytest.param(json1, json2, 'stylish', read(correct_diff)),
-                             pytest.param(yaml1, yaml2, 'stylish', read(correct_diff)),
-                             pytest.param(json3, json4, 'stylish', read(correct_stylish)),
-                             pytest.param(yaml3, yaml4, 'stylish', read(correct_stylish)),
-                             pytest.param(json3, json4, 'plain', read(correct_plain)),
-                             pytest.param(yaml3, yaml4, 'plain', read(correct_plain)),
+                             pytest.param(json1, json2, 'stylish', correct_diff),
+                             pytest.param(yaml1, yaml2, 'stylish', correct_diff),
+                             pytest.param(json3, json4, 'stylish', correct_stylish),
+                             pytest.param(yaml3, yaml4, 'stylish', correct_stylish),
+                             pytest.param(json3, json4, 'plain', correct_plain),
+                             pytest.param(yaml3, yaml4, 'plain', correct_plain),
                              pytest.param(json3, json4, 'json', json.dumps(json.load(open(correct_json)))),
                              pytest.param(yaml3, yaml4, 'json', json.dumps(json.load(open(correct_json))))
                          ]
                          )
 def test_generate_diff(test_input1, test_input2, formatter, expected):
-    assert generate_diff(test_input1, test_input2, formatter) == expected
+    with open(expected) as file:
+        expected_result = file.read()
+    assert generate_diff(test_input1, test_input2, formatter) == expected_result
