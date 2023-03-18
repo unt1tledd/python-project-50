06bb1556793afae0df19ab40405e1b1277aeeed2
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-18 08:33:29 +0300

    Create cli.py

---
+++ gendiff/cli.py
@@ -0,0 +1,7 @@
+def parse():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', default='stylish')
+    return parser.parse_args()
