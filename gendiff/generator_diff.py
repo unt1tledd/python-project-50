503f004d25002c5b24808e3adff3a2b0e255f150
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-20 19:52:07 +0300

    Update generator_diff.py

--- gendiff/generator_diff.py
+++ gendiff/generator_diff.py
@@ -5,12 +5,12 @@ from gendiff.formatters.plain import format_plain
 from gendiff.formatters.json import format_json
 
 
-def formatted(name_formater):
-    if name == 'stylish':
+def formatted(name_formater, diff):
+    if name_formatter == 'stylish':
         diff = f"{{\n{format_stylish(diff)}}}"
-    elif name == 'plain':
+    elif name_formatter == 'plain':
         diff = format_plain(diff)
-    elif name == 'json':
+    elif name_formatter == 'json':
         diff = format_json(diff)
     else:
         raise ValueError('This format is not supported:(')
@@ -20,5 +20,5 @@ def generate_diff(filepath1, filepath2, name_formatter='stylish'):
     file1 = get_data(filepath1)
     file2 = get_data(filepath2)
     diff = make_diff(file1, file2)
-    diff = formatted(name_formatter)
+    diff = formatted(name_formatter, diff)
     return diff
