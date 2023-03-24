15f718cb6a3d99b687c222cd529d55b6bc24bcec
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2023-03-24 22:58:58 +0300

    Update parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -5,9 +5,9 @@ import yaml
 def get_data(filepath):
     format = filepath.split('.')[1]
     if format == 'yaml' or format == 'yml':
-        file = yaml.safe_load(filepath)
+        file = yaml.safe_load(open(filepath))
     elif format == 'json':
-        file = json.load(filepath)
+        file = json.load(open(filepath))
     else:
         raise Exception("Unsupported type of file")
     return file
